20f81742fa9581cb7c59eadbf5b52ee40d914abc
RocherDouteux <f.willems42@gmail.com>
2025-04-13 14:24:19 +0200

    Update URL

--- MasiID/Domains/Network.cs
+++ MasiID/Domains/Network.cs
@@ -1,29 +1,31 @@
 ﻿using System;
-using System.CodeDom;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net.Http;
+using System.Security.Authentication;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
-using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace MasiID.Domains
 {
     public class Network
     {
-
-        private const string API_URL_REGISTER = "http://127.0.0.1:8080/register";
+        private const string API_URL_REGISTER = "https://192.168.8.55:30444/AMAZI/V1/mazi-id/registerTest";
 
         public static async Task<bool> SendDataToApiAsync(byte[] userData, byte[] signature, X509Certificate2 cert)
         {
-            var client = new HttpClient();
+            var handler = new HttpClientHandler()
+            {
+                SslProtocols = SslProtocols.Tls12 | SslProtocols.Tls11 | SslProtocols.Tls,
+                // Bypass certificate validation
+                ServerCertificateCustomValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true
+            };
+
+            var client = new HttpClient(handler);
 
             var requestMessage = new HttpRequestMessage(HttpMethod.Post, API_URL_REGISTER);
-
             requestMessage.Headers.Add("X-Data", Convert.ToBase64String(userData));
             requestMessage.Headers.Add("X-Signature", Convert.ToBase64String(signature));
             requestMessage.Headers.Add("X-Certificate", Convert.ToBase64String(cert.Export(X509ContentType.Cert)));
-           
 
             try
             {
@@ -36,5 +38,4 @@ namespace MasiID.Domains
             }
         }
     }
-
 }
